856444e96748a2680db551b7aa3d12886cffdd9f
ericdavidanderson <burrej1@gmail.com>
2021-11-29 01:30:18 -0800

    get routes for /notes and api/notes

--- server.js
+++ server.js
@@ -1,19 +1,39 @@
 //dependencies
+const { application } = require("express");
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const { Console } = require("console");
+
 
 //express route
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3003;
+const dbFile = "./db/db.json";
+
 
 //data parsing for express
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
 app.use(express.static("public"));
 
-//route requirements:
-require("./routes/apiRoutes")(app);
-require("./routes/homeRoutes")(app);
+
+app.get("/notes", (req,res) => {
+  console.log("/notes route, line 21");
+  res.sendFile(path.join(_dirname, "public/notes.html"))
+});
+
+app.get("/api/notes", (req,res) => {
+  fs.readFile(dbFile, "utf8", (error, data) =>{
+    if(error) {
+     console.log("/api/notes path error", dbFile, data, error);
+     res.status(500).send("Can not read file");
+    }
+    res.status(200).json(JSON.parse(data));
+  });
+});
+
+
 
 //set up local port
 app.listen(PORT, function () {
